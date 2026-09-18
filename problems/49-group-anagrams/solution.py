bca3c54819372ff23a3e190cda5c5c013b64241b
Matthew Serrano <180496361+matt-serrano@users.noreply.github.com>
2026-09-18 11:56:43 -0400

    Sync LeetCode: 49. Group Anagrams (python3)

---
+++ problems/49-group-anagrams/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = defaultdict(list) 
+
+        for s in strs:
+            count = [0] * 26 
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
